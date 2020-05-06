a6fcb74db58bc4be0dd9f5596fd7c6837edbb9c5
unknown <rajeshm@e-magic.ca>
2020-05-06 17:40:57 +0530

    solconfig

--- fileCreate.ps1
+++ fileCreate.ps1
@@ -1,1 +1,11 @@
-﻿New-Item -ItemType File -path C:\test1.txt
+﻿param(
+      [string]$iothubname,
+      [string]$iothubCompatiblePath
+    
+     
+  )
+New-Item -ItemType File -path C:\test1.txt
+$value=$iothubname+$iothubCompatiblePath
+
+Set-Content -Path C:\test1.txt -Value $value
+
