1df9a4615983bdd23325d2d795f02a35a46bf965
rajesh-emagic <53461573+rajesh-emagic@users.noreply.github.com>
2020-06-11 12:36:06 +0530

    Add files via upload

--- TwinWorXSolutionConfig.ps1
+++ TwinWorXSolutionConfig.ps1
@@ -7,101 +7,129 @@ param(
       [string]$s_iotHubSasKeyName,
       [string]$ConsumerGroup,
       [string]$eventhubname,
-      [string]$eventHubConnectionString
+      [string]$eventHubConnectionString,
+      [string]$postgreHost,
+      [string]$postgreUserName,
+      [string]$postgrePassword,
+      [string]$postgreDbName,
+      [string]$postgrePort,
+      [string]$vmIP
+      
      
   )
-New-Item -ItemType File -path C:\test1.txt
-$value=$iothubname+$iothubCompatibleEndpoint+$iothubCompatiblePath+$iothubs_iotHubSasKey+$s_iotHubSasKeyName+$ConsumerGroup+$eventhubname+$eventHubConnectionString
-
-Set-Content -Path C:\test1.txt -Value $value
-#----------------------get parameter---------------------
-
-# ----------------------iis installation---------------
-Install-WindowsFeature -Name Web-Server -IncludeManagementTools
-
-# ------------------hosting environment------------------------
-$temp_path = "C:\temp\"
-
-if( ![System.IO.Directory]::Exists( $temp_path ) )
-{
-
-   New-Item -ItemType Directory -Force -Path $temp_path
-  # Write-Output "Path not found ($temp_path), create the directory and try again"
-
-
-}
-
-
-#
-# Download the Windows Hosting Bundle Installer for ASP.NET Core 3.1 Runtime (v3.1.0)
-#
-# The installer URL was obtained from:
-# https://dotnet.microsoft.com/download/dotnet-core/thank-you/runtime-aspnetcore-3.1.0-windows-hosting-bundle-installer
-#
-
-$whb_installer_url = "https://download.visualstudio.microsoft.com/download/pr/fa3f472e-f47f-4ef5-8242-d3438dd59b42/9b2d9d4eecb33fe98060fd2a2cb01dcd/dotnet-hosting-3.1.0-win.exe"
-
-$whb_installer_file = $temp_path + [System.IO.Path]::GetFileName( $whb_installer_url )
-
-Try
-{
-
-   Invoke-WebRequest -Uri $whb_installer_url -OutFile $whb_installer_file
-
-   Write-Output ""
-   Write-Output "Windows Hosting Bundle Installer downloaded"
-   Write-Output "- Execute the $whb_installer_file to install the ASP.Net Core Runtime"
-   Write-Output ""
-   C:\temp\dotnet-hosting-3.1.0-win.exe  /quiet /install
-}
-Catch
-{
-
-   Write-Output ( $_.Exception.ToString() )
-
-   Break
-
-}
-
-#----------end hosting environment-----------------------
-
-#---------------Download TwinWorX solution---------------
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url= "https://github.com/rajesh-emagic/twservices/blob/master/TwinWorXServicesPublish.zip?raw=true"
-#$output = "D:\psfile\master.zip"
-# TwinWorXPublish
-$output = "C:\TwinWorXServicesPublish.zip"
-$start_time = Get-Date
-
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $output)
-
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)" 
-
-expand-archive -path $output -destinationpath 'C:\TwinWorXServicesPublish'
 
 
 #-------------End-------------------------
 
 #--------------------Change appsetting----------------------
-$TwinWorXDTPath = 'C:\TwinWorXServicesPublish\8.TwinWorXDT\appsettings.json'
-$TwinWorXDataResourceMangaerPath = 'C:\TwinWorXServicesPublish\2.TwinWorXDataResourceMangaer\appsettings.json'
-
+$TwinWorxUrl='http://'+$vmIP+':1009'
+$ApiGatewayUrl='http://'+$vmIP+':1007'
+$TwinWorXServiceRegisteryPath = 'C:\Emagic\TwinWorXApps\1.TwinWorXServiceRegistery\appsettings.json'
+$TwinWorXDataResourceMangaerPath = 'C:\Emagic\TwinWorXApps\2.TwinWorXDataResourceMangaer\appsettings.json'
+$TwinWorXDataAccessServicePath = 'C:\Emagic\TwinWorXApps\3.TwinWorXDataAccessService\appsettings.json'
+$TwinWorxDataAlarmingServicePath = 'C:\Emagic\TwinWorXApps\4.TwinWorxDataAlarmingService\appsettings.json'
+$TwinWorXHistoricalDataAccessServicePath = 'C:\Emagic\TwinWorXApps\5.TwinWorXHistoricalDataAccessService\appsettings.json'
+$TwinWorXAlarmingLoggerServicePath = 'C:\Emagic\TwinWorXApps\6.TwinWorXAlarmingLoggerService\appsettings.json'
+$TwinWorXApiGatewayPath = 'C:\Emagic\TwinWorXApps\7.TwinWorXApiGateway\appsettings.json'
+$TwinWorXDTPath = 'C:\Emagic\TwinWorXApps\8.TwinWorXDT\appsettings.json'
+$TwinWorXExplorerPath = 'C:\Emagic\TwinWorXSolution\wwwroot\ClientApp\assets\config\configuration.json'
 #----------------------------------------------rsmgr----------------------------------
-(Get-Content $TwinWorXDataResourceMangaerPath).replace('ehcon',$eventHubConnectionString) | Set-Content $TwinWorXDataResourceMangaerPath
-(Get-Content $TwinWorXDataResourceMangaerPath).replace('ehn',$eventhubname) | Set-Content $TwinWorXDataResourceMangaerPath
-(Get-Content $TwinWorXDataResourceMangaerPath).replace('cg',$consumerGroup) | Set-Content $TwinWorXDataResourceMangaerPath
+(Get-Content $TwinWorXDataResourceMangaerPath).replace('{eventhubconstring}',$eventHubConnectionString) | Set-Content $TwinWorXDataResourceMangaerPath
+(Get-Content $TwinWorXDataResourceMangaerPath).replace('{eventhubname}',$eventhubname) | Set-Content $TwinWorXDataResourceMangaerPath
+(Get-Content $TwinWorXDataResourceMangaerPath).replace('{cgp}',$consumerGroup) | Set-Content $TwinWorXDataResourceMangaerPath
 #--------------------------------------------DT---------------------------------------------
-(Get-Content $TwinWorXDTPath).replace('iothcep',$iothubCompatibleEndpoint) | Set-Content $TwinWorXDTPath
-(Get-Content $TwinWorXDTPath).replace('iothcp',$iothubCompatiblePath) | Set-Content $TwinWorXDTPath
-(Get-Content $TwinWorXDTPath).replace('iothsk',$iothubs_iotHubSasKey) | Set-Content $TwinWorXDTPath
-(Get-Content $TwinWorXDTPath).replace('iothsaskeyn',$s_iotHubSasKeyName) | Set-Content $TwinWorXDTPath
-(Get-Content $TwinWorXDTPath).replace('cg',$ConsumerGroup) | Set-Content $TwinWorXDTPath
-(Get-Content $TwinWorXDTPath).replace('ehcon',$eventHubConnectionString) | Set-Content $TwinWorXDTPath
-(Get-Content $TwinWorXDTPath).replace('ehn',$eventhubname) | Set-Content $TwinWorXDTPath
-#--------------------End------------------------------------
-#----------------install microservices--------------
+(Get-Content $TwinWorXDTPath).replace('{iothubcomendpoint}',$iothubCompatibleEndpoint) | Set-Content $TwinWorXDTPath
+(Get-Content $TwinWorXDTPath).replace('{iothubcompath}',$iothubCompatiblePath) | Set-Content $TwinWorXDTPath
+(Get-Content $TwinWorXDTPath).replace('{iothubskey}',$iothubs_iotHubSasKey) | Set-Content $TwinWorXDTPath
+(Get-Content $TwinWorXDTPath).replace('{iothubskname}',$s_iotHubSasKeyName) | Set-Content $TwinWorXDTPath
+(Get-Content $TwinWorXDTPath).replace('{cgp}',$ConsumerGroup) | Set-Content $TwinWorXDTPath
+(Get-Content $TwinWorXDTPath).replace('{eventhubconstring}',$eventHubConnectionString) | Set-Content $TwinWorXDTPath
+(Get-Content $TwinWorXDTPath).replace('{eventhubname}',$eventhubname) | Set-Content $TwinWorXDTPath
 
-& C:\TwinWorXServicesPublish\twxautodeployscript.ps1
+#------------------------------------pgsql---------------------------------------------------#
+(Get-Content $TwinWorXServiceRegisteryPath).replace('{host}',$postgreHost) | Set-Content $TwinWorXServiceRegisteryPath
+(Get-Content $TwinWorXServiceRegisteryPath).replace('{port}',$postgrePort) | Set-Content $TwinWorXServiceRegisteryPath
+(Get-Content $TwinWorXServiceRegisteryPath).replace('{username}',$postgreUserName) | Set-Content $TwinWorXServiceRegisteryPath
+(Get-Content $TwinWorXServiceRegisteryPath).replace('{password}',$postgrePassword) | Set-Content $TwinWorXServiceRegisteryPath
+(Get-Content $TwinWorXServiceRegisteryPath).replace('{database}',$postgreDbName) | Set-Content $TwinWorXServiceRegisteryPath
+
+(Get-Content $TwinWorXDataAccessServicePath).replace('{host}',$postgreHost) | Set-Content $TwinWorXDataAccessServicePath
+(Get-Content $TwinWorXDataAccessServicePath).replace('{port}',$postgrePort) | Set-Content $TwinWorXDataAccessServicePath
+(Get-Content $TwinWorXDataAccessServicePath).replace('{username}',$postgreUserName) | Set-Content $TwinWorXDataAccessServicePath
+(Get-Content $TwinWorXDataAccessServicePath).replace('{password}',$postgrePassword) | Set-Content $TwinWorXDataAccessServicePath
+(Get-Content $TwinWorXDataAccessServicePath).replace('{database}',$postgreDbName) | Set-Content $TwinWorXDataAccessServicePath
+
+(Get-Content $TwinWorxDataAlarmingServicePath).replace('{host}',$postgreHost) | Set-Content $TwinWorxDataAlarmingServicePath
+(Get-Content $TwinWorxDataAlarmingServicePath).replace('{port}',$postgrePort) | Set-Content $TwinWorxDataAlarmingServicePath
+(Get-Content $TwinWorxDataAlarmingServicePath).replace('{username}',$postgreUserName) | Set-Content $TwinWorxDataAlarmingServicePath
+(Get-Content $TwinWorxDataAlarmingServicePath).replace('{password}',$postgrePassword) | Set-Content $TwinWorxDataAlarmingServicePath
+(Get-Content $TwinWorxDataAlarmingServicePath).replace('{database}',$postgreDbName) | Set-Content $TwinWorxDataAlarmingServicePath
+
+(Get-Content $TwinWorXHistoricalDataAccessServicePath).replace('{host}',$postgreHost) | Set-Content $TwinWorXHistoricalDataAccessServicePath
+(Get-Content $TwinWorXHistoricalDataAccessServicePath).replace('{port}',$postgrePort) | Set-Content $TwinWorXHistoricalDataAccessServicePath
+(Get-Content $TwinWorXHistoricalDataAccessServicePath).replace('{username}',$postgreUserName) | Set-Content $TwinWorXHistoricalDataAccessServicePath
+(Get-Content $TwinWorXHistoricalDataAccessServicePath).replace('{password}',$postgrePassword) | Set-Content $TwinWorXHistoricalDataAccessServicePath
+(Get-Content $TwinWorXHistoricalDataAccessServicePath).replace('{database}',$postgreDbName) | Set-Content $TwinWorXHistoricalDataAccessServicePath
+
+(Get-Content $TwinWorXAlarmingLoggerServicePath).replace('{host}',$postgreHost) | Set-Content $TwinWorXAlarmingLoggerServicePath
+(Get-Content $TwinWorXAlarmingLoggerServicePath).replace('{port}',$postgrePort) | Set-Content $TwinWorXAlarmingLoggerServicePath
+(Get-Content $TwinWorXAlarmingLoggerServicePath).replace('{username}',$postgreUserName) | Set-Content $TwinWorXAlarmingLoggerServicePath
+(Get-Content $TwinWorXAlarmingLoggerServicePath).replace('{password}',$postgrePassword) | Set-Content $TwinWorXAlarmingLoggerServicePath
+(Get-Content $TwinWorXAlarmingLoggerServicePath).replace('{database}',$postgreDbName) | Set-Content $TwinWorXAlarmingLoggerServicePath
+
+(Get-Content $TwinWorXApiGatewayPath).replace('{clientUrl}',$TwinWorxUrl) | Set-Content $TwinWorXApiGatewayPath
+
+(Get-Content $TwinWorXExplorerPath).replace('{apiGatewayUrl}',$ApiGatewayUrl) | Set-Content $TwinWorXExplorerPath
+
+#--------------------End------------------------------------
+#------------------Firewall-----------------------------
+
+Import-Module NetSecurity
+New-NetFirewallRule -DisplayName "twcustomRule1" -Direction Inbound -LocalPort Any -Protocol TCP -Action Allow
+
+#-----------------------------------end--------------------
+#-----------------------restart pgserver---------------------
+<#
+
+Write-host "Grabbing Service Principal..."
+
+$response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' -Method GET -UseBasicParsing -Headers @{Metadata="true"}
+
+$content = $response.Content | ConvertFrom-Json
+
+$ArmToken = $content.access_token
+
+Write-host "The managed identities for Azure resources access token is $ArmToken"
+
+$subscriptions='4ddcea5c-502f-4395-b3dc-5b2e28da9cc4'
+$resourceGroup='emagic'
+$pgServer='pgdemo'
+$token='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkN0VHVoTUptRDVNN0RMZHpEMnYyeDNRS1NSWSIsImtpZCI6IkN0VHVoTUptRDVNN0RMZHpEMnYyeDNRS1NSWSJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuYXp1cmUuY29tLyIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0LzMyNzE3ZWZlLThmY2ItNDQ4Ni05MDExLTI3N2MyNjVjMGFjMi8iLCJpYXQiOjE1ODk5NzQwNTUsIm5iZiI6MTU4OTk3NDA1NSwiZXhwIjoxNTkwMDYwNzU1LCJhaW8iOiI0MmRnWUZoeFRTbDhQV2ZoazhoLzU1TlRGbmd2QXdBPSIsImFwcGlkIjoiMzdhYTgwYWItYTNmYS00OGQyLWI5ZmItNmU5N2Q0ZjZkYzg1IiwiYXBwaWRhY3IiOiIyIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvMzI3MTdlZmUtOGZjYi00NDg2LTkwMTEtMjc3YzI2NWMwYWMyLyIsIm9pZCI6IjZmYmFmZTYwLTJiNmMtNDg3ZS04YThiLWE3NGFlZTcyNjA3NCIsInN1YiI6IjZmYmFmZTYwLTJiNmMtNDg3ZS04YThiLWE3NGFlZTcyNjA3NCIsInRpZCI6IjMyNzE3ZWZlLThmY2ItNDQ4Ni05MDExLTI3N2MyNjVjMGFjMiIsInV0aSI6Ik1nZ2ZuZ0NXV1V1X1NuMnluMHhBQUEiLCJ2ZXIiOiIxLjAiLCJ4bXNfbWlyaWQiOiIvc3Vic2NyaXB0aW9ucy80ZGRjZWE1Yy01MDJmLTQzOTUtYjNkYy01YjJlMjhkYTljYzQvcmVzb3VyY2Vncm91cHMvZW1hZ2ljL3Byb3ZpZGVycy9NaWNyb3NvZnQuQ29tcHV0ZS92aXJ0dWFsTWFjaGluZXMvaXp0aWR0d1dpblZNIn0.PHXH44XmX2vIZNEqrtnSCPYoz022BQjQMzO4XcZxtUFTJ4g4ZXNrwsOaEJR_l4NmThvsTprQfreeT-w2qwJMX2jlUbRQjEUGv0Na2BCiBIjNSSUhnSTtHu4E1S06ayHjF3xvxrQWuZqtaVKVWnwwH9FzGwtM6PNVI6KWrCXGYn7U_RX7_FYcB7TcfJPmRZlr1TcqoXQXfTdh8SmD9s_DpmvypUxf6b5bcxRD2x_3wGk9DY3uaDLRWCMs1sIpIMQr2-ZaViNljqVhZiQKF36rIDyKU6tGxgHZsD-7gHQyNxDRmFERZyhFH0AwFzyYMINvyX2d12l9C320wqgQx6ANRA'
+$Header = @{
+    'Content-type' = 'application/json'
+     Authorization = $token
+}
+$uri='https://management.azure.com/subscriptions/'+$subscriptions+'/resourceGroups/'+$resourceGroup+'/providers/Microsoft.DBforPostgreSQL/servers/'+$pgServer+'/restart?api-version=2017-12-01'
+
+
+$restartResponse = Invoke-WebRequest $uri  -Headers $Header -Method 'POST'
+
+Start-Sleep -s 30
+Write-Host $restartResponse
+
+#>
+
+#--------------------------invoke services-------------------------------------
+<#
+Invoke-WebRequest -Uri 'http://localhost:1001/default'
+Invoke-WebRequest -Uri 'http://localhost:1002/default'
+Invoke-WebRequest -Uri 'http://localhost:1003/default'
+Invoke-WebRequest -Uri 'http://localhost:1004/default'
+Invoke-WebRequest -Uri 'http://localhost:1005/default'
+Invoke-WebRequest -Uri 'http://localhost:1006/default'
+Invoke-WebRequest -Uri 'http://localhost:1007/default'
+Invoke-WebRequest -Uri 'http://localhost:1008/default'
+#>
+
 
 #-----------------------End---------------------------
